8350cc2b6d1d8904600bd29c9946f79a11be279a
Eric Poilvet <github@poilvet.com>
2022-07-08 17:21:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ SIMULATION_TABLE = 'STG_SIMULATION'
 # Uses st.experimental_singleton to only run once.
 
 session = Session.builder.configs(st.secrets["snowflake"]).create()
-df_simul_pd = session.table('STG_SIMULATION').to_pandas()
+df_simul = session.table('STG_SIMULATION')
+df_simul_pd= df_simul.toPandas()
 
 def update_table(df, table_name):
     new_simul = session.create_dataframe(df)
